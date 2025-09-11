92422cff688896ed96b096487dcbb2dfbd847042
Jonathan <jonathan.mfm@gmail.com>
2025-09-11 10:19:30 +0200

    feat: ajouter le composant Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
+import logo from "../../assets/icons/logo-white.svg"; // logo blanc
+import "./_footer.scss";
+
 function Footer() {
   return (
-    <footer>
-      <p>© 2020 Kasa. All rights reserved</p>
+    <footer className="footer">
+      <div className="footer__wrapper container">
+        <img src={logo} alt="Logo Kasa" className="footer__logo" />
+        <p className="footer__text">© 2025 Kasa. All rights reserved</p>
+      </div>
     </footer>
   );
 }
