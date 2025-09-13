08f8a1dfcf15ebee4404fdda01c6ea9709e68a55
Jonathan <jonathan.mfm@gmail.com>
2025-09-13 07:56:32 +0200

    style: forcer le footer à rester collé en bas même avec peu de contenu

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import "./_layout.scss";
 
 function Layout({ children }) {
   return (
-    <>
+    <div className="layout">
       <Header />
-      <main>{children}</main>
+      <main className="layout__content">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
 
