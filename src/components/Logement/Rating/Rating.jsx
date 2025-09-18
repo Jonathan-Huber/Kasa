5970f9c5105103faa199043aab47a7cd551276b2
Jonathan <jonathan.mfm@gmail.com>
2025-09-18 11:10:59 +0200

    feat: ajouter les composants tags, rating et collapse et plusieurs informations sur la page Logement.
    - Création et intégration du composant Tag pour afficher les tags du logement
    - Création et intégration du composant Rating avec étoiles pleines/vides en fonction de la note
    - Ajout des sections Description et Équipements via le composant Collapse
    - Ajout de l’affichage des informations principales du logement (titre, localisation nom/photo de l'hôte)
    - Mise à jour du style SCSS pour la page Logement : organisation en blocs, responsive et gestion de la mise en page

---
+++ src/components/Logement/Rating/Rating.jsx
@@ -0,0 +1,20 @@
+import StarFull from "../../../assets/icons/star-full.svg";
+import StarEmpty from "../../../assets/icons/star-empty.svg";
+import "./_rating.scss";
+
+function Rating({ value, max = 5 }) {
+  return (
+    <div className="rating">
+      {Array.from({ length: max }).map((_, index) => (
+        <img
+          key={index}
+          className="rating__star"
+          src={index < value ? StarFull : StarEmpty}
+          alt={index < value ? "Étoile pleine" : "Étoile vide"}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Rating;
--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,6 +1,9 @@
 import { useParams, Navigate } from "react-router-dom";
 import logementsData from "../../data/logements.json";
 import Slideshow from "../../components/Logement/Slideshow/Slideshow";
+import Tag from "../../components/Logement/Tag/Tag";
+import Rating from "../../components/Logement/Rating/Rating";
+import Collapse from "../../components/shared/Collapse/Collapse";
 import "./_logement.scss";
 
 function Logement() {
@@ -17,6 +20,38 @@ function Logement() {
   return (
     <div className="logement container">
       <Slideshow images={logement.pictures} />
+      <div className="logement__info">
+        <div className="logement__info-bloc1">
+          <div className="logement__info-bloc1-top">
+            <h1 className="logement__info-bloc1-top-title">{logement.title}</h1>
+            <p className="logement__info-bloc1-top-location">
+              {logement.location}
+            </p>
+          </div>
+          <div className="logement__info-bloc1-tags">
+            {logement.tags.map((tag, index) => (
+              <Tag key={index} label={tag} />
+            ))}
+          </div>
+        </div>
+        <div className="logement__info-bloc2">
+          <Rating value={logement.rating} />
+          <div className="logement__info-bloc2-host">
+            <p className="logement__info-bloc2-host-name">
+              {logement.host.name}
+            </p>
+            <img
+              src={logement.host.picture}
+              alt={logement.host.name}
+              className="logement__info-bloc2-host-picture"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="logement__collapse">
+        <Collapse title="Description" content={logement.description} />
+        <Collapse title="Équipements" content={logement.equipments} />
+      </div>
     </div>
   );
 }
