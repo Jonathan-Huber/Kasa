49a0e78d86c1ff5709e796373b45df853ae3da6a
Jonathan <jonathan.mfm@gmail.com>
2025-09-23 14:11:57 +0200

    refactor: remplacer Link par NavLink pour la gestion automatique des liens actifs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import logo from "../../assets/icons/logo-primary.svg";
 import "./_header.scss";
 
 function Header() {
   const location = useLocation();
   const isHome = location.pathname === "/";
-  const isAbout = location.pathname === "/about";
 
   return (
     <header className="header container">
@@ -15,19 +14,17 @@ function Header() {
             <img src={logo} alt="Logo Kasa" />
           </h1>
         ) : (
-          <Link to="/">
+          <NavLink to="/">
             <img src={logo} alt="Logo Kasa" />
-          </Link>
+          </NavLink>
         )}
       </div>
 
       <nav className="header__nav">
-        <Link to="/" className={isHome ? "active" : ""}>
+        <NavLink to="/" end>
           Accueil
-        </Link>
-        <Link to="/about" className={isAbout ? "active" : ""}>
-          A Propos
-        </Link>
+        </NavLink>
+        <NavLink to="/about">A Propos</NavLink>
       </nav>
     </header>
   );
