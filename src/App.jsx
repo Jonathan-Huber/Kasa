e7c67773ffaeefcfe347eceb2eb20905becb96df
Jonathan <jonathan.mfm@gmail.com>
2025-09-24 12:07:16 +0200

    fix: ajouter le slash à la route 404

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/logement/:id" element={<Logement />} />
-          <Route path="404" element={<NotFound />} />
+          <Route path="/404" element={<NotFound />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
