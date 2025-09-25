6c1f46a0a694beb39baf6ebf1054072f2fccb5ed
Jonathan <jonathan.mfm@gmail.com>
2025-09-25 18:21:28 +0200

    refactor: fusionner thumbs-wrapper et thumbs en simplifiant JSX et CSS

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,28 +6,26 @@ import "./_home.scss";
 
 function Home() {
   return (
-    <>
+    <div className="home">
       <Banner image={homeBannerImage} alt="Chez vous, partout et ailleurs">
         Chez vous,
         <span className="banner__line-break"> partout et ailleurs</span>
       </Banner>
 
-      <div className="home container">
-        <div className="home__thumbs-wrapper">
-          <div className="home__thumbs">
-            {logements.map(({ id, cover, title }) => (
-              <Thumb
-                key={id}
-                id={id}
-                cover={cover}
-                title={title}
-                HeadingTag={"h3"}
-              />
-            ))}
-          </div>
+      <div className="container">
+        <div className="home__thumbs">
+          {logements.map(({ id, cover, title }) => (
+            <Thumb
+              key={id}
+              id={id}
+              cover={cover}
+              title={title}
+              HeadingTag={"h3"}
+            />
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
